01e6fa1d891b0fc64b548dadbcdf984486ee339a
Ariel-Canseco <18161156@itoaxaca.edu.mx>
2022-06-17 13:49:51 -0500

    Add files

--- Arbol.java
+++ Arbol.java
@@ -1,3 +1,4 @@
+/*construir un árbol binario equilibrado a partir de las claves 1075342264534152874201590, */
 public class Arbol{
     Nodo raiz;
     public Arbol(){
@@ -18,5 +19,64 @@ public class Arbol{
             this.padre = null;
             this.contenido = null;
         }
+    }
+
+    public void insertar(int llave, Object contenido){
+        Nodo nuevo = new Nodo(llave);
+        nuevo.contenido = contenido;
+        if(raiz == null){
+            raiz = nuevo;
+        }else{
+            Nodo actual = raiz;
+            Nodo padre;
+            while(true){
+                padre = actual;
+                if(llave < actual.llave){
+                    actual = actual.izquierda;
+                    if(actual == null){
+                        padre.izquierda = nuevo;
+                        nuevo.padre = padre;
+                        return;
+                    }
+                }else{
+                    actual = actual.derecha;
+                    if(actual == null){
+                        padre.derecha = nuevo;
+                        nuevo.padre = padre;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void tooString(){
+        if(raiz != null){
+            System.out.println(raiz.llave);
+            if(raiz.izquierda != null){
+                System.out.println(raiz.izquierda.llave);
+            }
+            if(raiz.derecha != null){
+                System.out.println(raiz.derecha.llave);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Arbol arbol = new Arbol();
+        /*Aún no se si esto esté bien xD*/
+        arbol.insertar(10, "10");
+        arbol.insertar(75, "75");
+        arbol.insertar(34, "34");
+        arbol.insertar(22, "22");
+        arbol.insertar(64, "64");
+        arbol.insertar(53, "53");
+        arbol.insertar(12, "12");
+        arbol.insertar(45, "45");
+        arbol.insertar(67, "67");
+        arbol.insertar(89, "89");
+        arbol.insertar(100, "100");
+
+        arbol.tooString();
     }
 }
